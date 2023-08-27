5e5ee7e0350f77bb802f64bba1ac4747d734ac0a
Justin Lee <justinslee201100@yahoo.com>
2023-08-27 14:47:02 -0600

    Finish prepend method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,20 +1,60 @@
 class LinkedList
-    attr_reader :head, :current_node
+    attr_reader :head
     def initialize
         @head = nil
-        @current_node = current_node
+        # @current_node = current_node
+        
     end
     def append(data)
         new_node = Node.new(data)
-        if @head = nil
+        if @head.nil?
            @head = new_node
         else 
             current_node =  @head
             until current_node.next_node.nil?
                 current_node = current_node.next_node 
-        end
+            end
           current_node.next_node = new_node
         end
+    end
+    def count
+        i = 0
+        if @head.nil?
+            i = 0
+        elsif current_node = @head
+            i = 1
+            until current_node.next_node.nil?
+                current_node = current_node.next_node 
+                i += 1
+            end
+            i
+            
+        end
+    end
+    def to_string 
+        current_node = @head
+        node_house = ""
+        if @head.nil?
+            "This is empty"
+        else 
+            while current_node != nil
+            node_house += "#{current_node.data} "
+            current_node = current_node.next_node 
+            end 
+        end
+        node_house.strip
+    end
+    def prepend(data)
+        new_node = Node.new(data)
+        if @head.nil?
+            @head = new_node
+         else 
+            new_node.next_node = @head
+            @head = new_node
+         end
+    end
+    def incert(position, string)
+        
     end
 end
 
--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -28,5 +28,72 @@ RSpec.describe LinkedList do
       expect(list.head).to be_instance_of(Node)
     end
   end
+  describe 'count' do
+    it 'counts nodes in list' do
+      list = LinkedList.new
+      list.append("doop")
+      
+      
+      expect(list.count).to eq(1)
+    end
+  end
 
+  describe 'to string' do
+    it 'shows list of node data in a string' do
+      list = LinkedList.new
+      list.append("doop")
+      
+      
+      expect(list.to_string).to eq("doop")
+    end
+  end
+
+  describe 'methods work with multiple nodes' do
+    it 'checks methods work with multiple' do
+      list = LinkedList.new
+
+      expect(list.head).to eq(nil)
+      list.append("doop")
+      
+      expect(list.head.data).to eq("doop")
+      expect(list.head.next_node).to eq(nil)
+      list.append("deep")
+      
+      expect(list.count).to eq(2)
+      expect(list.to_string).to eq("doop deep")
+    end
+  end
+
+  describe 'multiple nodes' do
+    it 'adds multiple nodes' do
+    list = LinkedList.new
+    list.append("plop")
+  
+    expect(list.to_string).to eq("plop")
+    list.append("suu")
+
+    expect(list.to_string).to eq("plop suu") 
+    end
+  end
+  describe 'prepend' do
+    it 'adds node to front of list' do
+    list = LinkedList.new
+    list.append("plop")
+    list.append("suu")
+    list.prepend("dop")
+
+    expect(list.to_string).to eq("dop plop suu")
+    expect(list.count).to eq(3)
+    end
+  end
+  describe 'incert' do
+    it 'incert node where i want' do
+    list = LinkedList.new
+    list.append("plop")
+    list.append("suu")
+    list.prepend("dop")
+
+    expect(list.insert(1, "woo")).to eq("dop woo plop suu")
+    end
+  end
 end
